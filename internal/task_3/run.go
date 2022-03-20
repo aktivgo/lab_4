5e7bc66fc4f7e8ddae4f8cd31887159fc3c5e10f
aktivgo <aktivgo01062001@gmail.com>
2022-03-20 21:11:28 +0300

    Complete task 3

--- internal/task_3/run.go
+++ internal/task_3/run.go
@@ -1,6 +1,97 @@
 package task_3
 
+import (
+	"fmt"
+	"lab_4/internal/types"
+	"log"
+	"math/rand"
+	"sync"
+	"time"
+)
+
 func Run() error {
+	var storageSize int
+	fmt.Print("Введите размер хранилища: ")
+	_, _ = fmt.Scanf("%d\n", &storageSize)
+
+	storage := types.NewStorage(storageSize)
+
+	var countWorkers int
+	fmt.Print("Введите количество потоков-рабочих: ")
+	_, _ = fmt.Scanf("%d\n", &countWorkers)
+
+	var workerSpeed int
+	fmt.Print("Введите скорость работы потока-рабочего(в миллисекундах): ")
+	_, _ = fmt.Scanf("%d\n", &workerSpeed)
+
+	var countWorkerIterations int
+	fmt.Print("Введите количество итераций потока-рабочего: ")
+	_, _ = fmt.Scanf("%d\n", &countWorkerIterations)
+
+	var countReaders int
+	fmt.Print("Введите количество потоков-читателей: ")
+	_, _ = fmt.Scanf("%d\n", &countReaders)
+
+	var readerSpeed int
+	fmt.Print("Введите скорость работы потока-читателя(в миллисекундах): ")
+	_, _ = fmt.Scanf("%d\n", &readerSpeed)
+
+	var countReaderIterations int
+	fmt.Print("Введите количество итераций потока-читателя: ")
+	_, _ = fmt.Scanf("%d\n", &countReaderIterations)
+	fmt.Println()
+
+	var wgWorker sync.WaitGroup
+	wgWorker.Add(countWorkers)
+
+	for i := 0; i < countWorkers; i++ {
+		workerNumber := i + 1
+		go func() {
+			defer wgWorker.Done()
+
+			for j := 0; j < countWorkerIterations; j++ {
+				time.Sleep(time.Duration(workerSpeed) * time.Millisecond)
+
+				index := rand.Intn(storageSize - 1)
+				value := 1 + rand.Intn(8)
+
+				if err := storage.Inc(index, value); err != nil {
+					log.Printf("Рабочий №%d: %s\n", workerNumber, err)
+				}
+
+				log.Printf("Рабочий №%d: итерация %d, индекс %d, значение инкремента %d\n", j+1, workerNumber, index, value)
+			}
+		}()
+	}
+
+	var wgReader sync.WaitGroup
+	wgReader.Add(countReaders)
+
+	for i := 0; i < countReaders; i++ {
+		readerNumber := i + 1
+		go func() {
+			defer wgReader.Done()
+
+			for j := 0; j < countReaderIterations; j++ {
+				time.Sleep(time.Duration(readerSpeed) * time.Millisecond)
+
+				index := rand.Intn(storageSize - 1)
+
+				var value int
+				value, err := storage.Get(index)
+				if err != nil {
+					log.Printf("Читатель №%d: %s\n", readerNumber, err)
+				}
+
+				log.Printf("Читатель №%d: итерация %d, индекс = %d, значение = %d\n", j+1, readerNumber, index, value)
+			}
+		}()
+	}
+
+	wgWorker.Wait()
+	wgReader.Wait()
+
+	fmt.Println("\nПрограмма завершила работу")
 
 	return nil
 }
--- internal/types/storage.go
+++ internal/types/storage.go
@@ -1,11 +1,58 @@
 package types
 
+import (
+	"fmt"
+	"sync"
+)
+
 type Storage struct {
+	sync.Mutex
 	Data []int
 }
 
-func NewStorage(data []int) *Storage {
+func NewStorage(size int) *Storage {
+	data := initData(size)
+
 	return &Storage{
 		Data: data,
 	}
+}
+
+func initData(size int) []int {
+	var data []int
+
+	for i := 0; i < size; i++ {
+		data = append(data, 0)
+	}
+
+	return data
+}
+
+func (s *Storage) Inc(index int, value int) error {
+	s.Lock()
+	defer s.Unlock()
+
+	if err := s.validateIndex(index); err != nil {
+		return err
+	}
+
+	s.Data[index] = value
+
+	return nil
+}
+
+func (s *Storage) Get(index int) (int, error) {
+	if err := s.validateIndex(index); err != nil {
+		return 0, err
+	}
+
+	return s.Data[index], nil
+}
+
+func (s *Storage) validateIndex(index int) error {
+	if index < 0 || index >= len(s.Data) {
+		return fmt.Errorf("%d: выход за пределы массива (%d; %d)", index, 0, len(s.Data))
+	}
+
+	return nil
 }
